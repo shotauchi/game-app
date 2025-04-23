878b249169f0b1fa36c49e10d4f343c5de7a88d3
shotauchi <tianneixiang84@gmail.com>
2025-04-23 14:27:20 +0000

    database-02

---
+++ database/migrations/2025_04_23_134025_create_consoles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('consoles', function (Blueprint $table) {
+            $table->id();
+            $table->text('introduction'); 
+            $table->string('Manufacturer');
+            $table->string('use');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('consoles');
+    }
+};
