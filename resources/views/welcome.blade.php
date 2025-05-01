e3af5df9c96851042c05ce58fd44a1f7b1b9c6b8
shotauchi <tianneixiang84@gmail.com>
2025-05-01 14:11:16 +0000

    bootstrap-03

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,52 +23,87 @@
     <body class="antialiased">　
         
         <!-- ナビバー -->
-<nav class="navbar bg-light d-flex justify-content-between px-3">
-    <div class="d-flex align-items-center">
-        <div class="rounded-circle bg-warning d-flex justify-content-center align-items-center" style="width: 50px; height: 50px;">
-            ロゴ
+        <nav class="navbar bg-light d-flex justify-content-between px-3">
+            <div class="d-flex align-items-center">
+                <div class="rounded-circle bg-warning d-flex justify-content-center align-items-center" style="width: 50px; height: 50px;">
+                    ロゴ
+                </div>
+            </div>
+            <div class="d-flex align-items-center">
+                <div class="rounded-circle bg-warning d-flex justify-content-center align-items-center" style="width: 50px; height: 50px;">
+                    ユーザー
+                </div>
+            </div>
+        </nav>
+        
+        <!-- サイト名 -->
+        <div class="text-center my-4">
+            <h1>サイト名</h1>
         </div>
-    </div>
-    <div class="d-flex align-items-center">
-        <div class="rounded-circle bg-warning d-flex justify-content-center align-items-center" style="width: 50px; height: 50px;">
-            ユーザー
-        </div>
-    </div>
-</nav>
-
+        
         <!-- パネル3つ -->
-<div class="container">
-    <div class="row mb-4">
-        <div class="col-8 offset-2">
-            <div class="card bg-primary" style="height: 200px;"></div>
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col-8 offset-2">
+                    <div class="card">
+                        <div class="card-header bg-success d-flex justify-content-between align-items-center">
+                            <span class="text-white fw-bold">ジャンル検索</span>
+                            <form class="d-flex" role="search">
+                              <input class="form-control me-2" type="search" placeholder="検索" aria-label="Search">
+                              <button class="btn btn-light" type="submit">Search</button>
+                            </form>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">フィルターで中質したゲーム1</li>
+                                <li class="list-group-item">フィルターで中質したゲーム2</li>
+                                <li class="list-group-item">フィルターで中質したゲーム3</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-4">
+                <div class="col-8 offset-2">
+                    <div class="card">
+                        <div class="card-header bg-primary d-flex justify-content-between align-items-center">
+                            <span class="text-white fw-bold">ゲームタイトル検索</span>
+                            <form class="d-flex" role="search">
+                              <input class="form-control me-2" type="search" placeholder="検索" aria-label="Search">
+                              <button class="btn btn-light" type="submit">Search</button>
+                            </form>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">タイトル検索でヒットしたゲーム1</li>
+                                <li class="list-group-item">タイトル検索でヒットしたゲーム2</li>
+                                <li class="list-group-item">タイトル検索でヒットしたゲーム3</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-4">
+                <div class="col-8 offset-2">
+                    <div class="card">
+                        <div class="card-body">
+                          <h5 class="card-title text-center">サイト案内</h5>
+                          <ul class="list-group list-group-numbered list-group-flush">
+                            <li class="list-group-item">ゲーム一覧ページ</li>
+                            <li class="list-group-item">コンソール一覧ページ</li>
+                            <li class="list-group-item">パフォーマンス一覧ページ</li>
+                          </ul>
+                        </div>
+                      </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
-    <div class="row mb-4">
-        <div class="col-8 offset-2">
-            <div class="card bg-primary" style="height: 200px;"></div>
+<!-- フッター -->
+        <footer class="bg-light mt-auto py-4 px-3 d-flex justify-content-end">
+        <div class="rounded-circle bg-warning d-flex justify-content-center align-items-center" style="width: 70px; height: 70px;">
+            戻る
         </div>
-    </div>
-    <div class="row mb-4">
-        <div class="col-8 offset-2">
-            <div class="card bg-primary" style="height: 200px;"></div>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-　　　
-　　　<!-- トップページ -->
-<top class="top bg-light d-flex justify-content-between px-3">
-    <div class="d-flex align-items-center">
-        <div class="rounded-circle bg-warning d-flex justify-content-center align-items-center" style="width: 50px; height: 50px;">
-            トップページへ
-        </div>
-    </div>
-</top>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
