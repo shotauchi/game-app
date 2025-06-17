19740cc5437a8fe009345a683fcaabaa461394a8
shotauchi <tianneixiang84@gmail.com>
2025-06-17 14:35:12 +0000

    console_table-01

--- resources/views/consoles/index.blade.php
+++ resources/views/consoles/index.blade.php
@@ -5,14 +5,31 @@
         <div class="text-center my-4">
             <h1>コンソール一覧画面です</h1>
         </div>
-        
-        <ul>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">introduction</th>
+      <th scope="col">Manufacturer</th>
+      <th scope="col">use</th>
+    </tr>
+  </thead>
+  <tbody>
     @forelse ($consoles as $console)
-        <li>{{ $console->use }}</li>
+    <tr>
+      <th scope="row">{{ $loop->iteration }}</th>
+      <td>{{ $console->introduction }}</td>
+      <td>{{ $console->Manufacturer }}</td>
+      <td>{{ $console->use }}</td>
+    </tr>
     @empty
-        <li>コンソールがありません。</li>
+    <tr>
+      <th scope="row">-</th>
+      <td>コンソールがありません。</td>
+      <td>コンソールがありません。</td>
+    </tr>
     @endforelse
-</ul>
-        
+  </tbody>
+</table>
         
 @endsection
