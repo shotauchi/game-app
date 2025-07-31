11ba676ec96e86bbfd6603c66b51ba6daf2586a4
shotauchi <tianneixiang84@gmail.com>
2025-07-31 03:39:11 +0000

    games_app-blade-layout-02

--- resources/views/games/edit.blade.php
+++ resources/views/games/edit.blade.php
@@ -49,35 +49,5 @@
         </div>
     </div>
 </div>
-<div class="text-center my-5">
-    <h1>編集画面一覧です。</h1>
-    
-    <!-- 編集フォーム -->
-    <form action="{{ route('games.update', $game->id) }}" method="POST">
-        @csrf
-        @method('PUT') <!-- PUTメソッドに変換（更新処理） -->
-    
-    <div class="mb-3">
-            <label for="image" class="form-label">画像</label>
-            <input type="text" name="image" id="image" class="form-control" value="{{ old('image', $game->image) }}" required>
-        </div>
-        
-        <div class="mb-3">
-            <label for="url" class="form-label">URL</label>
-            <input type="text" name="url" id="url" class="form-control" value="{{ old('url', $game->url) }}" required>
-        </div>
-        
-        <div class="mb-3">
-            <label for="site" class="form-label">サイト名</label>
-            <input type="text" name="site" id="site" class="form-control" value="{{ old('site', $game->site) }}" required>
-        </div>
-        
-        <div class="mb-3">
-            <label for="introduction" class="form-label">紹介文</label>
-            <textarea name="introduction" id="introduction" class="form-control" rows="5" required>{{ old('introduction', $game->introduction) }}</textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">更新する</button>
-    </form>
-</div>
         
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,14 +37,14 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas offcanvas-end bg-primary" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
           <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasNavbarLabel">ユーザー</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div class="offcanvas-body">
             <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li class="nav-item">
+              <li class="nav-item bg-success">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li class="nav-item">
@@ -87,5 +87,11 @@
         </footer>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+      body {
+        padding-top: 60px;
+        padding-button: 60px;
+      }
+    </style>
 </body>
 </html>
