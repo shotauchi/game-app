0128916779a844a54c9172ddc9a07879ddefc272
shotauchi <tianneixiang84@gmail.com>
2025-08-09 18:39:21 +0000

    Material_Symbols-02

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -36,30 +36,33 @@
 <!-- 変更後 -->
 <div class="d-flex justify-content-center gap-3 my-4">
     <div class="d-flex justify-content-center gap-3 my-4">
-    <a href="{{ route('games.index') }}" class="btn btn-sm btn-outline-primary">ゲーム一覧</a>
-    <a href="{{ route('consoles.index') }}" class="btn btn-sm btn-outline-warning">コンソール一覧</a>
-    <a href="{{ route('performances.index') }}" class="btn btn-sm btn-outline-success">パフォーマンス一覧</a>
+    <a href="{{ route('games.index') }}" class="btn btn-sm btn-outline-primary"><span class="material-symbols-outlined fs-6">stadia_controller</span>ゲーム一覧</a>
+    <a href="{{ route('consoles.index') }}" class="btn btn-sm btn-outline-warning"><span class="material-symbols-outlined fs-6">keyboard</span>コンソール一覧</a>
+    <a href="{{ route('performances.index') }}" class="btn btn-sm btn-outline-success"><span class="material-symbols-outlined fs-6">select_all</span>パフォーマンス一覧</a>
     </div>
 </div>
 <div class="d-flex justify-content-center align-items-center vh-100">
     <div class="d-flex flex-column gap-3 text-center">
      <span class="material-symbols-outlined fs-1">star_rate_half</span>
-         <a href="{{ route('games.create') }}" class="btn btn-primary">
+        <a href="{{ route('games.create') }}" class="btn btn-primary">
+        <span class="material-symbols-outlined fs-5">stadia_controller</span>
         新しいゲームを作成
         </a>
         
         <a href="{{ route('consoles.create') }}" class="btn btn-warning">
+        <span class="material-symbols-outlined fs-5">keyboard</span>
         新しいコンソールを作成
         </a>
         
         <a href="{{ route('performances.create') }}" class="btn btn-success">
+        <span class="material-symbols-outlined fs-5">select_all</span>
         新しいパフォーマンスを作成
         </a>
         
         <form method="POST" action="{{ route('admin.logout') }}" class="d-inline">
             @csrf
             <button type="submit" class="btn btn-outline-danger">
-            ログアウト
+            <span class="material-symbols-outlined fs-6">exit_to_app</span>ログアウト
             </button>
         </form>
     </div>    
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,18 +39,18 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="offcanvas offcanvas-end bg-primary" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas offcanvas-end bg-warning" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
           <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">ユーザー</h5>
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><span class="material-symbols-outlined fs-5">account_box</span>ユーザー</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div class="offcanvas-body">
             <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li class="nav-item bg-success">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"><span class="material-symbols-outlined fs-6">home</span>Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#"><span class="material-symbols-outlined fs-6">link</span>Link</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -83,7 +83,7 @@
     <footer class="bg-white mt-auto py-4 px-3 d-flex justify-content-end">
         <a href="/admin">
           <div style="position: fixed; bottom: 20px; right: 20px; background-color: gold; border-radius: 50%; width: 60px; height: 60px; display: flex; align-items: center; justify-content: center; font-weight: bold;">
-            戻る 
+            <span class="material-symbols-outlined fs-6">subdirectory_arrow_left</span>戻る 
           </div>
         </a>
     </footer>
