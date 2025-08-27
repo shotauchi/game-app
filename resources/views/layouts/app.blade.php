ff343cd9979f37f41f11db5f487ed689e19df130
shotauchi <tianneixiang84@gmail.com>
2025-08-27 17:49:01 +0000

    app.blade.php_button_change-04

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,15 +79,16 @@
     <main class="py-4 flex-grow-1 min-vh-100">
         @yield('content')
     </main>
-    <!-- フッター -->
-    <footer class="bg-white mt-auto py-4 px-3 d-flex justify-content-end">
-        <a href="/admin">
-          <div style="position: fixed; bottom: 20px; right: 20px; background-color: gold; border-radius: 50%; width: 60px; height: 60px; display: flex; align-items: center; justify-content: center; font-weight: bold;">
-            <span class="material-symbols-outlined fs-6">subdirectory_arrow_left</span>戻る 
-          </div>
-        </a>
-    </footer>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+     {{-- フッター --}}
+@unless (request()->routeIs('home'))
+<footer class="bg-white mt-auto py-4 px-3 d-flex justify-content-end">
+    <a href="{{ route('home') }}">
+      <div style="position: fixed; bottom: 20px; right: 20px; background-color: gold; border-radius: 50%; width: 60px; height: 60px; display: flex; align-items: center; justify-content: center; font-weight: bold;">
+        <span class="material-symbols-outlined fs-6">subdirectory_arrow_left</span>戻る
+      </div>
+    </a>
+</footer>
+@endunless
+
 </body>
 </html>
